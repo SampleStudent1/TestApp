d51c3624db7db15b99c0edfdfdff3c87ac1af792
Sample Student <eleanorgamgee@yahoo.com>
2014-09-09 13:49:15 +0000

    Updates tables as test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829230803) do
+ActiveRecord::Schema.define(version: 20140903212203) do
+
+  create_table "sample_tables", force: true do |t|
+    t.string "sample"
+  end
 
   create_table "test_pages", force: true do |t|
     t.datetime "created_at"
