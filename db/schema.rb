e8935509f59964a395be4b6ce071b9c9bd1840b3
Sample Student <eleanorgamgee@yahoo.com>
2014-09-15 16:53:57 +0000

    Updates for nested form- ads within magazines

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903212203) do
+ActiveRecord::Schema.define(version: 20140909140303) do
+
+  create_table "ads", force: true do |t|
+    t.integer  "magazine_id"
+    t.string   "vendor"
+    t.string   "copy"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "magazines", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "sample_tables", force: true do |t|
     t.string "sample"
